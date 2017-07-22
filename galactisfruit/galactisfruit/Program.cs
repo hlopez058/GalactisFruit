e24303ce540d456c67bfd2368ae39a6a012df4c0
Hector Lopez <hlopez058@gmail.com>
2017-07-21 20:22:34 -0400

    working perlin generation
    simple image of a map using perlin.cs from forked github.
    https://gist.github.com/Flafla2/f0260a861be0ebdeef76.js

---
+++ galactisfruit/galactisfruit/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galactisfruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Galactis Fruit - Generate Planet Textures");
+
+
+            //inputs : solar system information
+
+
+            //create map with white noise
+            //image size
+            var size = 256;
+            var map = new double[size,size];
+            
+            //change texture with perlin noise
+            var noise = new Perlin();
+            var white_noise = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    double ni = (i + 1) * .00390625; // (1/256)
+                    double nj = (j + 1) * .00390625;
+
+                    map[i, j] = noise.OctavePerlin(ni, nj, 0,9,0.75);
+                    
+                    //map[i, j] = noise.perlin(ni, nj, 0);
+
+                    //map[i, j] = white_noise.Next(0, 100)*0.01;
+                }
+            }
+            
+            //publish map to an image
+            Publish.CreateImage("map.jpg",map);
+            
+            //create image file for new planet texture
+        }
+    }
+}
---
+++ galactisfruit/galactisfruit/Publish.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace galactisfruit
+{
+    class Publish
+    {
+        public static void CreateImage(string filename, double[,] map)
+        {
+            System.Drawing.Bitmap flag = new System.Drawing.Bitmap(map.GetLength(0), map.GetLength(1));
+
+            for (int x = 0; x < flag.Height; ++x)
+            {
+                for (int y = 0; y < flag.Width; ++y)
+                {
+                    var N = scaleBetween(map[x, y], 0, 255, 0, 1);
+                    flag.SetPixel(x, y, Color.FromArgb(N, 0, 0));
+                }
+            }
+
+            
+            flag.Save(filename, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        private static int scaleBetween(double unscaledNum, double minAllowed, double maxAllowed,double  min,double max)
+        {
+            return Convert.ToInt16((maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed);
+        }
+    }
+}
